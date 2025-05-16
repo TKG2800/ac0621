b6467ea0faac865df3a14fd601794d843d4a98fe
Jirokichi1987 <cappyyoshi@gmail.com>
2025-05-16 23:52:09 +0900

    fixed memo.ts

--- tutorial/memo.ts
+++ tutorial/memo.ts
@@ -18,7 +18,8 @@ const creatememo = () => {
   return memo;
 } 
 const removememo = () => {
-
+  
+  memolist.pop(searchmemo());
 }
 const updatememo = () => {
 
@@ -26,4 +27,18 @@ const updatememo = () => {
 const savememo = () => {
   memolist.push (creatememo());
 }
-
+const searchmemo = (findid: number) => {
+  let i: number; 
+  for (i=0, i<memolist.length; i++;) {
+    if (memolist[i].id == findid){
+      console.log(memolist[i].id);
+      return memolist[i];
+    }
+  
+  }
+}
+const diaplaymemo = () => {
+  let list = document.getElementById("list") as HTMLGetElement | null;
+  if (list instanceof HTMLGetElement)
+  
+}
